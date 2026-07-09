9e75ab453ae9609562048edb62e1ff3283986950
kimsolpapa <go1you@icloud.com>
2026-07-09 16:51:30 +0900

    Import FE web map controller

--- backend/fe-web/src/main/java/kr/go/tkjf/JobabaMapFeWebApplication.java
+++ backend/fe-web/src/main/java/kr/go/tkjf/JobabaMapFeWebApplication.java
@@ -3,8 +3,10 @@ package kr.go.tkjf;
 import kr.go.tkjf.usr.map.controller.MapController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackageClasses = {JobabaMapFeWebApplication.class, MapController.class})
+@Import(MapController.class)
 public class JobabaMapFeWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(JobabaMapFeWebApplication.class, args);
