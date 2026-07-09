ebc7c4e490fb53eb7e80107b0c5abf5de5aa378d
kimsolpapa <go1you@icloud.com>
2026-07-09 16:53:29 +0900

    Support FE web WAR deployment

--- backend/fe-web/src/main/java/kr/go/tkjf/JobabaMapFeWebApplication.java
+++ backend/fe-web/src/main/java/kr/go/tkjf/JobabaMapFeWebApplication.java
@@ -3,12 +3,19 @@ package kr.go.tkjf;
 import kr.go.tkjf.usr.map.controller.MapController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackageClasses = {JobabaMapFeWebApplication.class, MapController.class})
 @Import(MapController.class)
-public class JobabaMapFeWebApplication {
+public class JobabaMapFeWebApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(JobabaMapFeWebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JobabaMapFeWebApplication.class);
     }
 }
